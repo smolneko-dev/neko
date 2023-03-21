62bc58b63733e35db0b2cc80fe48288aa0e48436
Vasily Yenin <vasily.yenin@gmail.com>
2023-03-22 05:48:52 +0700

    chore: Fix error text
    Just copy and paste... :D

--- internal/usecase/characters.go
+++ internal/usecase/characters.go
@@ -20,7 +20,7 @@ func NewCharacters(r CharactersRepo) *CharactersUseCase {
 func (uc CharactersUseCase) Characters(ctx context.Context, count int, cursor string) ([]model.Character, string, string, error) {
 	characters, next, prev, err := uc.repo.GetCharacters(ctx, count, cursor)
 	if err != nil {
-		return nil, "", "", fmt.Errorf("CharactersUseCase - Characters - f.repo.GetCharacters: %w", err)
+		return nil, "", "", fmt.Errorf("CharactersUseCase - Characters - uc.repo.GetCharacters: %w", err)
 	}
 
 	return characters, next, prev, nil
@@ -29,7 +29,7 @@ func (uc CharactersUseCase) Characters(ctx context.Context, count int, cursor st
 func (uc CharactersUseCase) Character(ctx context.Context, id string) (model.Character, error) {
 	character, err := uc.repo.GetCharacterById(ctx, id)
 	if err != nil {
-		return character, fmt.Errorf("CharactersUseCase - Character - f.repo.GetCharacter: %w", err)
+		return character, fmt.Errorf("CharactersUseCase - Character - uc.repo.GetCharacter: %w", err)
 	}
 
 	return character, nil
